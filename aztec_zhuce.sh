e386a701317d0b62557ef3b754130286f92a1d32
chenxishun <1007422542@qq.com>
2025-07-21 20:43:24 +0800

    Update aztec_zhuce.sh

--- aztec_zhuce.sh
+++ aztec_zhuce.sh
@@ -80,7 +80,7 @@ const ABI = [
   const contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, wallet);
 
   const gasLimit = 12000000;
-  const gasPrice = ethers.parseUnits("90", "gwei"); // 自定义 gas
+  const gasPrice = ethers.parseUnits("658", "gwei"); // 自定义 gas
 
   try {
     console.log("🚀 正在发送 addValidator...");
